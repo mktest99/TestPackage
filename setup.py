334b07b2ff87b98915fdfe034338b5dd3a7f431a
MK <mk@test.com>
2018-03-09 13:01:54 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='testpkg',
+    packages=['TestPackage'],
+    description='Hello world',
+    version='0.1',
+    url='http://github.com/mktest99/testpkg',
+    author='mk',
+    author_email='mk@example.com',
+    keywords=['pip','example']
+    )
